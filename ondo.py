4b2eb2a0a64c4a57a5aab39f0615f31e47ad5712
shoma564 <shomaigu@gmail.com>
2022-05-05 16:00:59 +0900

    second

--- ondo.py
+++ ondo.py
@@ -1,9 +1,9 @@
-import requests,pprint,json,time,os, prometheus_client, threading
-from prometheus_client import start_http_server, Summary
 from datetime import datetime
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+from random import randrange
 from urllib.parse import parse_qs, urlparse
-
+import requests,pprint,json,time, prometheus_client, threading, time, os, random
+from prometheus_client import start_http_server, Summary
 
 url = "https://api.webstorage.jp/v1/devices/current"
 kami1 = ""
@@ -20,7 +20,16 @@ paylord = {'api-key':api_key,"login-id":login_id,'login-pass':password}
 
 header = {'Host':'api.webstrage.js:443','Content-Type': 'application/json','X-HTTP-Method-Override':'GET'}
 
-def main():
+
+class MyHTTPRequestHandler(BaseHTTPRequestHandler):
+    global kami1, kami2, kami3, temp
+    def do_GET(self):
+        global kami1, kami2, kami3, temp
+        parsed_path = urlparse(self.path)
+
+        if parsed_path.path.endswith('/error'):
+            raise Exception('Error')
+
         response = requests.post(url,json.dumps(paylord).encode('utf-8'),headers=header).json()
         global kami1, kami2, kami3
         print(response['devices'][0]['channel'][0]['value'], end='')
@@ -32,28 +41,21 @@ def main():
         kami1 = float(response['devices'][0]['channel'][0]['value'])
         kami2 = float(response['devices'][1]['channel'][0]['value'])
         kami3 = float(response['devices'][2]['channel'][0]['value'])
-
-
-
-def server():
-    start_http_server(8000)
-    with ThreadingHTTPServer(('0.0.0.0', 8080), kami1) as server:
-        print(f'[{datetime.now()}] Server startup.') 
+        
+        
         temp.labels('unit1').set(kami1)
         temp.labels('unit2').set(kami2)
         temp.labels('unit3').set(kami3)
 
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/plain; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(f'再収集 from {self.path} as GET'.encode('utf-8'))
+        time.sleep(1)
+
+if __name__ == '__main__':
+    start_http_server(8000)
+
+    with ThreadingHTTPServer(('0.0.0.0', 8001), MyHTTPRequestHandler) as server:
+        print(f'[{datetime.now()}] Server startup.')
         server.serve_forever()
-
-
-
-
-if __name__ =="__main__":
-    main()
-    thread1 = threading.Thread(target=main)
-    thread2 = threading.Thread(target=server)
-    thread2.start()
-
-    while True:
-        time.sleep(15)
-        main()
